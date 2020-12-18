8ecc57a95637502c2f86f5aaf31808794193c3cf
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2020-12-18 17:19:18 -0600

    Updates

---
+++ precache-manifest.cfb604d43793e6466fce8cb58fd3a639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d365aee73ebe4bb0004e7fc5a677b270",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "20efe79fc1eeb210961c",
+    "url": "/react-recipe-app/static/css/main.f391bded.chunk.css"
+  },
+  {
+    "revision": "a1135f3790e476ae6f29",
+    "url": "/react-recipe-app/static/js/2.b45f8366.chunk.js"
+  },
+  {
+    "revision": "20efe79fc1eeb210961c",
+    "url": "/react-recipe-app/static/js/main.194fbabc.chunk.js"
+  },
+  {
+    "revision": "e9ed7916298e18513132",
+    "url": "/react-recipe-app/static/js/runtime~main.f6d3a2cf.js"
+  },
+  {
+    "revision": "80ea3c27532d5accc72650d2f2f214a0",
+    "url": "/react-recipe-app/static/media/food.80ea3c27.jpg"
+  }
+]);
